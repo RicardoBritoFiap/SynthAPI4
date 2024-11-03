f7d0a87da07f84ca8ddbdd8b14a04f17f4771f43
RicardoBritoFiap <rbponticceli@gmail.com>
2024-11-03 15:49:37 -0300

    API in

---
+++ synthia/src/main/java/com/sprint4/synthia/SynthiaApplication.java
@@ -0,0 +1,13 @@
+package com.sprint4.synthia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynthiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynthiaApplication.class, args);
+	}
+
+}
---
+++ synthia/src/test/java/com/sprint4/synthia/SynthiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint4.synthia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynthiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
