0196e378a8b3be486d86226d1d4d537d472a8ab9
RicardoBritoFiap <rbponticceli@gmail.com>
2024-11-04 19:27:37 -0300

    c

--- synthia/src/main/java/com/fiap/synthia/empresa/token/SecurityConfig.java
+++ synthia/src/main/java/com/fiap/synthia/empresa/token/SecurityConfig.java
@@ -3,7 +3,10 @@ package com.fiap.synthia.empresa.token;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,6 +31,11 @@ public class SecurityConfig {
                         .permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
